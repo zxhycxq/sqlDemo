78beb34664a02d55c5ee39e91f5109a7caa91502
zxhycxq <lcyxlqn@163.com>
2019-08-07 20:44:48 +0800

    express-todo

---
+++ express-todo/controllers/todoController.js
@@ -0,0 +1,13 @@
+
+module.exports=function (app) {
+  app.get('./todo',function (req,res) {
+  
+  })
+  
+  app.post('/todo',function () {
+  
+  })
+  app.delete('/todo',function () {
+  
+  })
+}
