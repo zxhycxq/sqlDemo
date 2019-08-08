f9bd71527fd9988a268f39303a5736c2e1232b3b
zxhycxq <lcyxlqn@163.com>
2019-08-08 22:08:44 +0800

    服务端-todo

--- express-todo/controllers/todoController.js
+++ express-todo/controllers/todoController.js
@@ -1,13 +1,20 @@
+var bodyParser=require('body-parser')
+var data=[{item:'asdf'},{item:'asdf222'}]
 
-module.exports=function (app) {
-  app.get('./todo',function (req,res) {
-  
+var urlencodeParser=bodyParser.urlencoded({extended:false});
+module.exports = function (app) {
+  app.get ('/todo', function (req, res) {
+    res.render('todo',{todos:data})
   })
   
-  app.post('/todo',function () {
-  
+  app.post ('/todo',urlencodeParser, function (req,res) {
+    data.push(req.body)
+    res.json(data)
   })
-  app.delete('/todo',function () {
-  
+  app.delete ('/todo/:item', function (req,res) {
+    data=data.filter(function (todo) {
+      return todo.item.replace(/ /g,'-')!=req.params.item;
+    })
+    res.json(data)
   })
 }
