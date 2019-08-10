9d9f9d514b73a9144f51a752319b2661d60dddea
zxhycxq <lcyxlqn@163.com>
2019-08-10 17:31:40 +0800

    服务端-todo

--- express-todo/app.js
+++ express-todo/app.js
@@ -1,6 +1,23 @@
 var express = require ('express')
 
 var todoController = require ('./controllers/todoController')
+var mongoose = require ('mongoose')
+mongoose.connect ('mongodb://zxhy:zxhy.mlab.com:51068/todos')
+
+var todoSchema = new mongoose.Schema ({
+  item: String
+});
+
+var Todo=mongoose.model('Todo',todoSchema);
+var itemOne=Todo({
+  item:'asdf'
+}).save(function (err) {
+  if(err){
+    throw new Error('Error description', 'app');
+  }else{
+    console.log(`%c--666-- `, 'color:blue;', 666)
+  }
+})
 var app = express ()
 
 app.set ('view engine', 'ejs')
--- express-todo/controllers/todoController.js
+++ express-todo/controllers/todoController.js
@@ -1,20 +1,25 @@
-var bodyParser=require('body-parser')
-var data=[{item:'asdf'},{item:'asdf222'}]
+var bodyParser = require ('body-parser')
+var data = [{item: 'asdf'}, {item: 'asdf222'}]
 
-var urlencodeParser=bodyParser.urlencoded({extended:false});
+var urlencodeParser = bodyParser.urlencoded ({extended: false});
 module.exports = function (app) {
   app.get ('/todo', function (req, res) {
-    res.render('todo',{todos:data})
+    Todo.find({},function (err,data) {
+      if(err){ throw new Error('Error description', 'todoController');
+        res.render ('todo', {todos: data})
+      }
+    })
+    
   })
   
-  app.post ('/todo',urlencodeParser, function (req,res) {
-    data.push(req.body)
-    res.json(data)
+  app.post ('/todo', urlencodeParser, function (req, res) {
+    data.push (req.body)
+    res.json (data)
   })
-  app.delete ('/todo/:item', function (req,res) {
-    data=data.filter(function (todo) {
-      return todo.item.replace(/ /g,'-')!=req.params.item;
+  app.delete ('/todo/:item', function (req, res) {
+    data = data.filter (function (todo) {
+      return todo.item.replace (/ /g, '-') != req.params.item;
     })
-    res.json(data)
+    res.json (data)
   })
 }
